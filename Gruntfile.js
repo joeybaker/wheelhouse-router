3843fd096c8ea2bc24c9b0b03568f74c3f0bcc1c
Joey Baker <joey@byjoeybaker.com>
2013-04-16 16:43:24 -0700

    RM old file, Don't lint compiled file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,6 +16,7 @@ module.exports = function(grunt){
         , 'index.js'
         , 'lib/**/*.js'
         , 'test/**/*.js'
+        , '!**/**.compiled.js'
       ]
       , options: {
         jshintrc: '.jshintrc'
