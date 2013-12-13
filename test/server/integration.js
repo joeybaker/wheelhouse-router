0efde138bf5034be39a7c81d2bbee4a4db2ff095
Joey Baker <joey.baker@ubnt.com>
2013-12-12 16:38:14 -0800

    Add unit tests for server router

--- test/server/integration.js
+++ test/server/integration.js
@@ -1,4 +1,4 @@
-/*global describe, it, before */
+/*global describe, it, before, after */
 'use strict';
 var app = require('../fixtures/app')
   , request = require('request')
@@ -35,90 +35,94 @@ describe('router', function(){
     })
   })
 
-  describe('/', function(){
-    it('renders the home controller', function(done){
+  describe('visiting', function(){
+    it('/ renders the home controller', function(done){
       request.get('http://localhost:' + port, function(err, res, body){
         body.should.exist
         body.indexOf('"view":"home"').should.not.equal(-1)
         done()
       })
     })
-  })
 
-  describe('/streets', function(){
-    var url = 'http://localhost:' + port + '/streets'
-    describe('/', function(){
-      it('renders the street controller with the index action', function(done){
-        request.get(url, function(err, res, body){
-          body.should.exist
-          body.indexOf('"view":"streets/index"').should.not.equal(-1)
-          done()
-        })
-      })
-    })
-
-    describe('/:id', function(){
-      it('renders the street controller with the detail action', function(done){
-        request.get(url + '/1', function(err, res, body){
-          body.should.exist
-          body.indexOf('"view":"streets/detail"').should.not.equal(-1)
-          done()
-        })
-      })
-    })
-
-    describe('/:id/edit/', function(){
-      it('renders the street controller with the edit action', function(done){
-        request.get(url + '/1/edit/', function(err, res, body){
-          body.should.exist
-          body.indexOf('"view":"streets/edit"').should.not.equal(-1)
-          done()
-        })
-      })
-    })
-  })
-
-  describe('/api', function(){
     describe('/streets', function(){
-      var url = 'http://localhost:' + port + '/api/streets'
-        , id
-      it('POSTs JSON', function(done){
-        request.post({
-          url: url
-          , json: {name: 'test-name2'}
-        }, function(err, res, body){
-          if (err) throw err
-          res.statusCode.should.equal(200)
-          id = body.id
-          body.should.exist
-          done()
+      var url = 'http://localhost:' + port + '/streets'
+      describe('/', function(){
+        it('renders the street controller with the index action', function(done){
+          request.get(url, function(err, res, body){
+            body.should.exist
+            body.indexOf('"view":"streets/index"').should.not.equal(-1)
+            done()
+          })
         })
       })
-      it('PUTs JSON', function(done){
-        request.put({
-          url: url + '/' + encodeURIComponent(id)
-          , json: {name: 'test-revised-again'}
-        }, function(err, res, body){
-          should.exist(body)
-          res.statusCode.should.equal(200)
-          done()
+
+      describe('/:id', function(){
+        it('renders the street controller with the detail action', function(done){
+          request.get(url + '/1', function(err, res, body){
+            body.should.exist
+            body.indexOf('"view":"streets/detail"').should.not.equal(-1)
+            done()
+          })
         })
       })
-      it('GETs JSON', function(done){
-        request.get(url, function(err, res, body){
-          res.statusCode.should.equal(200)
-          body.should.exist
-          done()
-        })
-      })
-      it('DELETEs via an id', function(done){
-        request.del(url + '/' + encodeURIComponent(id), function(err, res){
-          if (err) console.error(err)
-          res.statusCode.should.equal(200)
-          done()
+
+      describe('/:id/edit/', function(){
+        it('renders the street controller with the edit action', function(done){
+          request.get(url + '/1/edit/', function(err, res, body){
+            body.should.exist
+            body.indexOf('"view":"streets/edit"').should.not.equal(-1)
+            done()
+          })
         })
       })
     })
+
+    describe('/api', function(){
+      describe('/streets', function(){
+        var url = 'http://localhost:' + port + '/api/streets'
+          , id
+        it('POSTs JSON', function(done){
+          request.post({
+            url: url
+            , json: {name: 'test-name2'}
+          }, function(err, res, body){
+            if (err) throw err
+            res.statusCode.should.equal(200)
+            id = body.id
+            body.should.exist
+            done()
+          })
+        })
+        it('PUTs JSON', function(done){
+          request.put({
+            url: url + '/' + encodeURIComponent(id)
+            , json: {name: 'test-revised-again'}
+          }, function(err, res, body){
+            should.exist(body)
+            res.statusCode.should.equal(200)
+            done()
+          })
+        })
+        it('GETs JSON', function(done){
+          request.get(url, function(err, res, body){
+            res.statusCode.should.equal(200)
+            body.should.exist
+            done()
+          })
+        })
+        it('DELETEs via an id', function(done){
+          request.del(url + '/' + encodeURIComponent(id), function(err, res){
+            if (err) console.error(err)
+            res.statusCode.should.equal(200)
+            done()
+          })
+        })
+      })
+    })
+  })
+
+  after(function(done){
+    app.server.close(done)
   })
 })
 
---
+++ test/server/unit.js
@@ -0,0 +1,155 @@
+'use strict';
+/*global describe, it, before, after */
+
+var app = require('../fixtures/app')
+  , plugin = require('../../index.js')
+  , chai = require('chai')
+  , expect = chai.expect
+  , sinon = require('sinon')
+  , sinonChai = require('sinon-chai')
+  , context = {
+    req: {}
+    , res: {
+      end: function(){}
+      , writeHead: function(){}
+    }
+  }
+
+chai.use(sinonChai)
+
+describe('router', function(){
+  var port = app.config.get('port')
+
+  before(function(done){
+    app.options.log = {console: {silent: true}}
+    app.start(port, done)
+  })
+
+  describe('respondWithError', function(){
+    var fn = plugin.internals.respondWithError
+
+    it('logs the stack trace', function(){
+      var status = 404
+        , log = sinon.spy(app.log, 'warn')
+
+      fn.call(context, status)
+      expect(log).to.have.been.called
+      // get the second arg of the first call
+      expect(log.args[0][1].stack).to.be.an.array
+
+      log.restore()
+    })
+
+    it('logs the user', function(){
+      var status = 404
+        , log = sinon.spy(app.log, 'warn')
+
+      fn.call(context, status)
+      expect(log).to.have.been.called
+      // get the second arg of the first call
+      expect(log.args[0][1].user).to.be.a.string
+
+      log.restore()
+    })
+
+    it('sets the status code', function(){
+      var status = 404
+
+      sinon.spy(context.res, 'writeHead')
+
+      fn.call(context, status)
+      expect(context.res.writeHead).to.have.been.calledOnce
+      expect(context.res.writeHead.args[0][0]).to.equal(status)
+
+      context.res.writeHead.restore()
+    })
+
+    it('responds with the error template', function(){
+      var status = 404
+      plugin.internals.options.err[status] = '404'
+      sinon.spy(plugin.internals.options, 'render')
+      sinon.spy(context.res, 'end')
+
+      fn.call(context, status)
+
+      expect(plugin.internals.options.render).to.have.been.calledOnce
+      expect(context.res.end).to.have.been.calledOnce
+
+      plugin.internals.options.render.restore()
+      delete plugin.internals.options.err[status]
+      context.res.end.restore()
+    })
+
+    it('responds with the error code if no template is specified', function(){
+      var status = 404
+      sinon.spy(plugin.internals.options, 'render')
+      sinon.spy(context.res, 'end')
+
+      expect(plugin.internals.options.err[status]).to.not.exist
+
+      fn.call(context, status)
+
+      expect(context.res.end).to.have.been.calledOnce
+
+      plugin.internals.options.render.restore()
+      context.res.end.restore()
+    })
+  })
+
+  describe('getRequestUserJSON', function(){
+    it('returns JSON if the request has a user object')
+    it('returns undefined if the request doesn\'t have a user')
+  })
+
+  describe('parseControllerMethod', function(){
+    it('runs the method with the collection')
+    it('catches errors if the method throws')
+    it('returns an object if the method is an object')
+    it('returns the model if the method is undefined')
+    it('returns the collection if the method and model are undefined')
+  })
+
+  describe('buildTemplate', function(){
+    it('calls the optionally passed render method')
+    it('calls #render if no render method was set')
+    it('calls render with a template')
+    it('calls render with data')
+  })
+
+  describe('render', function(){
+    it('builds the template with the collection if it is present in app.collections')
+    it('fetches data for the collection if the collection doesn\'t exist on the app')
+    it('builds the template without a collection if none is defined')
+    it('doesn\'t set headers if they\'ve already been set')
+    it('sets a 200 status code')
+    it('calls res.end')
+  })
+
+  describe('mountRoutes', function(){
+    it('attaches routes to the app router')
+    it('creates a route that returns a 403 on optional permissions returning false')
+    it('redirects if a 30* method is passed')
+    it('it calls render for GET requests')
+  })
+
+  describe('exports.name', function(){
+    it('equals routerPlugin', function(){
+      expect(plugin.name).to.equal('routerPlugin')
+    })
+  })
+
+  describe('exports.attach', function(){
+    it('parses the options')
+    it('attaches the options to internals')
+    it('mounts the routes')
+    it('logs the route on every request')
+    it('adds options.before to the router before array')
+    it('adds options.on to the router on array')
+    it('notfound returns a 404 page')
+  })
+
+  after(function(done){
+    app.server.close(done)
+  })
+})
+
